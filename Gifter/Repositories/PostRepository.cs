4c6836571e410cbc2b3568b3dcd9a62581222442
Jon Newton <jonn@hey.com>
2021-05-27 10:18:57 -0400

    Created Post repository and interface, created DBUtils.cs to abstract common ADO.NET functions, removed default API project files.

---
+++ Gifter/Repositories/PostRepository.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Gifter.Models;
+using Gifter.Utils;
+
+namespace Gifter.Repositories
+{
+				public class PostRepository : BaseRepository, IPostRepository
+				{
+								public PostRepository(IConfiguration configuration) : base(configuration) { }
+
+								public List<Post> GetAll()
+								{
+												using (SqlConnection conn = Connection)
+												{
+																conn.Open();
+																using (SqlCommand cmd = conn.CreateCommand())
+																{
+																				cmd.CommandText = @"
+																								SELECT		p.Id AS PostId,
+																																p.Title,
+																																p.Caption,
+																																p.DateCreated AS PostDateCreated,
+																																p.ImageUrl AS PostImageUrl,
+																																p.UserProfileId,
+
+																																up.[Name], 
+																																up.Bio, 
+																																up.Email, 
+																																up.DateCreated AS UserProfileDateCreated, 
+																																up.ImageUrl AS UserProfileImageUrl
+																								FROM Post AS p
+																												LEFT JOIN UserProfile up ON p.UserProfileId = up.id
+																								ORDER BY p.DateCreated
+																				";
+
+																				SqlDataReader reader = cmd.ExecuteReader();
+
+																				List<Post> posts = new List<Post>();
+																				while (reader.Read())
+																				{
+																								posts.Add(new Post()
+																								{
+																												Id = DbUtils.GetInt(reader, "PostId"),
+																												Title = DbUtils.GetString(reader, "Title"),
+																												Caption = DbUtils.GetString(reader, "Caption"),
+																												DateCreated = DbUtils.GetDateTime(reader, "PostDateCreated"),
+																												ImageUrl = DbUtils.GetString(reader, "PostImageUrl"),
+																												UserProfileId = DbUtils.GetInt(reader, "UserProfileId"),
+																												UserProfile = new UserProfile()
+																												{
+																																Id = DbUtils.GetInt(reader, "UserProfileId"),
+																																Name = DbUtils.GetString(reader, "Name"),
+																																Email = DbUtils.GetString(reader, "Email"),
+																																DateCreated = DbUtils.GetDateTime(reader, "UserProfileDateCreated"),
+																																ImageUrl = DbUtils.GetString(reader, "UserProfileImageUrl"),
+																												}
+																								});
+																				}
+
+																				reader.Close();
+
+																				return posts;
+																}
+
+												}
+								}
+
+								public Post GetById(int id)
+								{
+												using (var conn = Connection)
+												{
+																conn.Open();
+																using (var cmd = conn.CreateCommand())
+																{
+																				cmd.CommandText = @"
+                          SELECT Title, Caption, DateCreated, ImageUrl, UserProfileId
+                            FROM Post
+                           WHERE Id = @Id";
+
+																				DbUtils.AddParameter(cmd, "@Id", id);
+
+																				var reader = cmd.ExecuteReader();
+
+																				Post post = null;
+																				if (reader.Read())
+																				{
+																								post = new Post()
+																								{
+																												Id = id,
+																												Title = DbUtils.GetString(reader, "Title"),
+																												Caption = DbUtils.GetString(reader, "Caption"),
+																												DateCreated = DbUtils.GetDateTime(reader, "DateCreated"),
+																												ImageUrl = DbUtils.GetString(reader, "ImageUrl"),
+																												UserProfileId = DbUtils.GetInt(reader, "UserProfileId"),
+																								};
+																				}
+
+																				reader.Close();
+
+																				return post;
+																}
+												}
+								}
+
+								public void Add(Post post)
+								{
+												using (var conn = Connection)
+												{
+																conn.Open();
+																using (var cmd = conn.CreateCommand())
+																{
+																				cmd.CommandText = @"
+                        INSERT INTO Post (Title, Caption, DateCreated, ImageUrl, UserProfileId)
+                        OUTPUT INSERTED.ID
+                        VALUES (@Title, @Caption, @DateCreated, @ImageUrl, @UserProfileId)";
+
+																				DbUtils.AddParameter(cmd, "@Title", post.Title);
+																				DbUtils.AddParameter(cmd, "@Caption", post.Caption);
+																				DbUtils.AddParameter(cmd, "@DateCreated", post.DateCreated);
+																				DbUtils.AddParameter(cmd, "@ImageUrl", post.ImageUrl);
+																				DbUtils.AddParameter(cmd, "@UserProfileId", post.UserProfileId);
+
+																				post.Id = (int)cmd.ExecuteScalar();
+																}
+												}
+								}
+
+								public void Update(Post post)
+								{
+												using (var conn = Connection)
+												{
+																conn.Open();
+																using (var cmd = conn.CreateCommand())
+																{
+																				cmd.CommandText = @"
+                        UPDATE Post
+                           SET Title = @Title,
+                               Caption = @Caption,
+                               DateCreated = @DateCreated,
+                               ImageUrl = @ImageUrl,
+                               UserProfileId = @UserProfileId
+                         WHERE Id = @Id";
+
+																				DbUtils.AddParameter(cmd, "@Title", post.Title);
+																				DbUtils.AddParameter(cmd, "@Caption", post.Caption);
+																				DbUtils.AddParameter(cmd, "@DateCreated", post.DateCreated);
+																				DbUtils.AddParameter(cmd, "@ImageUrl", post.ImageUrl);
+																				DbUtils.AddParameter(cmd, "@UserProfileId", post.UserProfileId);
+																				DbUtils.AddParameter(cmd, "@Id", post.Id);
+
+																				cmd.ExecuteNonQuery();
+																}
+												}
+								}
+
+								public void Delete(int id)
+								{
+												using (var conn = Connection)
+												{
+																conn.Open();
+																using (var cmd = conn.CreateCommand())
+																{
+																				cmd.CommandText = "DELETE FROM Post WHERE Id = @Id";
+																				DbUtils.AddParameter(cmd, "@id", id);
+																				cmd.ExecuteNonQuery();
+																}
+												}
+								}
+				}
+}
